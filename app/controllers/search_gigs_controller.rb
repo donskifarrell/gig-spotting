9aea0d5cc99dafa47402696e9e8b542b16ddc149
donskifarrell <donskifarrell@googlemail.com>
2012-09-07 00:26:10 +0100

    Refactored rails search code to return proper result set. Added Parallel gem but not using it yet.

--- app/controllers/search_gigs_controller.rb
+++ app/controllers/search_gigs_controller.rb
@@ -9,6 +9,7 @@ class SearchGigsController < ApplicationController
 	# GET /search.json
 	def search
 		$log.info "SearchGigsController -> Index"
+		$gigs = []
 
 		if params.include? :artist 
 			@search = Search.new(
@@ -16,63 +17,84 @@ class SearchGigsController < ApplicationController
 				:location => params[:location],
 				:date => params[:date]
 			).save
+
 			$log.info " - Searching for artist: " + params[:artist]
-			results = $songkick.search_artists(params[:artist])
+			searchResults = $songkick.search_artists(params[:artist])
 
-			artistList = getArtistList(results)
-			#$log.info 'Artists List: ' + artistList.inspect
+			artists = getArtists(searchResults)
+			artists.each do |artist|
+				artistName = artist['displayName']
+				$log.info ' -- Artist Name: ' + artistName.inspect
+				artistEvents = getArtistEvents(artist)
+				next if artistEvents.nil?
 
-			# Assume index 0 is matching artist for now
-			displayName = getAttributeValue(artistList[0], 'displayName')
-			$log.info ' - Display Name: ' + displayName.inspect
+				# need to better format output
+				$gigs << { 
+					'artistName' => artistName,
+					'events' => getEventDetails(artistEvents)
+				}
+			end
 
-			links = getAttributeValue(artistList[0], 'identifier')
-			mbid = getAttributeValue(links[0], 'mbid')
-			$log.info ' - mbid: ' + mbid.inspect
-
-
-
-			events = $songkick.artist_calendar('mbid:'+mbid)
-			#$log.info 'Events: ' + events.inspect
-
-			eventList = getEventList(events)
-			#$log.info 'Event List: ' + eventList.inspect
-
-			location = getAttributeValue(eventList[0], 'location')
-			lat = location['lat']
-			lng = location['lng']
-
-			@gig = Gig.new(
-				:artist => displayName,
-				:lat => lat,
-				:lng => lng
-			)
-			@gig.save
-
-			$log.info ' - Gig info: ' + @gig.to_json
-		else
-			@gig = Gig.new(
-				:artist => "Dummy"
-			)
-			@gig.save
+			$log.info ' - Gigs: ' + $gigs.inspect
 		end
 
 		respond_to do |format|
 			format.json  {
-				render :json => @gig.to_json
+				render :json => $gigs.to_json
 			}
 		end 
 	end
 
-	def getArtistList(resultSet)
-		resultSet['resultsPage']['results']['artist']
+	def getArtistEvents(artist)
+		# It appears that all event links for an artist produce the same calendar,
+		# so we only really need the first one.
+		eventLink = getArtistEventLinks(artist)[0]
+		if eventLink.nil?		
+			$log.info ' --- Event mbid NIL'
+			$log.info ' --- Artist Detail: ' + artist.inspect
+			return
+		end
+		$log.info ' --- Event mbid: ' + eventLink
+
+		eventResults = $songkick.artist_calendar('mbid:' + eventLink)
+		return getEvents(eventResults)
+	end
+
+	def getArtistEventLinks(artist)
+		mbids = Array.new
+		artist['identifier'].each do |link|
+			mbids << link['mbid']
+		end
+		return mbids
+	end
+
+	def getEventDetails(events)
+		eventDetails = []
+		events.each do |event|
+			eventDetail = 
+				{
+					'displayName' => event['displayName'],
+					'markerLocation' => [ 
+						event['location']['lat'], 
+						event['location']['lng'] 
+					]
+				}
+
+			$log.info ' ---- Event: ' + eventDetail.to_json
+			eventDetails << eventDetail
+		end
+		return eventDetails
+	end
+
+	def getArtists(resultSet)
+		artistSet = resultSet['resultsPage']['results']['artist']		
+		#$log.info ' --- Artists: ' + artistSet.inspect
+		return artistSet
 	end	
 
-	def getEventList(resultSet)
-		resultSet['resultsPage']['results']['event']
-	end	
-
-	def getAttributeValue(hashObject, attributeName)
-		hashObject[attributeName]
-	end	
+	def getEvents(resultSet)
+		eventsSet = resultSet['resultsPage']['results']['event']		
+		#$log.info ' --- Events: ' + eventsSet.inspect
+		return eventsSet
+	end
 end
