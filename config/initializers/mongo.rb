6e883b4cc86de86e310d47cb353e69f472f15a02
donskifarrell <donskifarrell@googlemail.com>
2012-09-17 23:44:34 +0100

    Debugging heroku issues

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,15 +1,18 @@
 Rails.logger.info('######################## TESTSSTS!')
 Rails.logger.info('#############')
 Rails.logger.info(Rails.env.production?)
+puts '######################## TESTSSTS!'
+puts '#############'
+puts Rails.env.production?
 
 if Rails.env.production?
-	Rails.logger.info('########################  In production!')
-	Rails.logger.info(Rails.env.production?)
-	Rails.logger.info(Rails.env.production)
+	puts '########################  In production!'
+	puts Rails.env.production?
+	puts Rails.env.production
 	MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
 	MongoMapper.database = ENV['MONGOLAB_DB']
 else
-	Rails.logger.info('########################  Not in production mode!')
+	puts '########################  Not in production mode!'
 	MongoMapper.connection = Mongo::Connection.new('localhost', 27020)
 	MongoMapper.database = "#myapp-#{Rails.env}"
 end
