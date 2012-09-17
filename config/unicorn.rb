9b61322fc3d687d04528c01ab9fd17a1bc8a90b3
donskifarrell <donskifarrell@googlemail.com>
2012-09-17 16:37:04 +0100

    Fixing issues with Unicorn and heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,1 +1,9 @@
-listen 3000, :tcp_nopush => false
+if Rails.env.production?
+	port = ENV["PORT"].to_i
+else
+	port = 3000
+end
+
+listen port, :tcp_nopush => false
+worker_processes 4 # amount of unicorn workers to spin up
+timeout 30         # restarts workers that hang for 30 seconds
