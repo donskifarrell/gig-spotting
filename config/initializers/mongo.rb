19304df7ec3e369615f760a0802eb8b07addbe3f
donskifarrell <donskifarrell@googlemail.com>
2012-09-17 17:03:44 +0100

    Fixing issues with Unicorn and heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -6,6 +6,6 @@ if ENV["RAILS_ENV"] == 'production'
 	MongoMapper.database = ENV['MONGOLAB_DB']
 else
 	Rails.logger.info('########################  Not in production mode!')
-	MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+	MongoMapper.connection = Mongo::Connection.new('localhost', 27020)
 	MongoMapper.database = "#myapp-#{Rails.env}"
 end
